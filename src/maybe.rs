5b87ec8c05931b177c0005c923e20b01d271c197
Alice Cecile <alice.i.cecile@gmail.com>
2024-10-16 20:56:48 -0400

    Add hooks for Maybe

--- src/maybe.rs
+++ src/maybe.rs
@@ -1,8 +1,27 @@
-use bevy_ecs::{bundle::Bundle, component::Component};
+use core::marker::PhantomData;
+
+use bevy_ecs::{
+    component::{ComponentHooks, ComponentId, StorageType},
+    prelude::*,
+    world::{Command, DeferredWorld},
+};
 
 /// A component that when added to an entity, will be removed from the entity and replaced with its contents if [`Some`].
-#[derive(Component, Clone, Copy, Debug, PartialEq, Eq, Hash)]
+///
+/// Under the hood, this is done using component lifecycle hooks.
+/// The component is removed from the entity when it is added, and contents are extracted.
+/// If the inner value is [`Some`], the contents are then readded to the entity.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Maybe<B: Bundle>(pub Option<B>);
+
+impl<B: Bundle> Component for Maybe<B> {
+    /// This is a sparse set component as it's only ever added and removed, never iterated over.
+    const STORAGE_TYPE: StorageType = StorageType::SparseSet;
+
+    fn register_component_hooks(hooks: &mut ComponentHooks) {
+        hooks.on_add(maybe_hook::<B>);
+    }
+}
 
 impl<B: Bundle> Maybe<B> {
     /// Creates a new `Maybe` component of type `B` with no bundle.
@@ -23,5 +42,45 @@ impl<B: Bundle> Default for Maybe<B> {
     /// Defaults to [`Maybe::NONE`].
     fn default() -> Self {
         Self::NONE
+    }
+}
+
+/// A hook that runs whenever [`Maybe`] is added to an entity.
+///
+/// Generates a [`MaybeCommand`].
+fn maybe_hook<B: Bundle>(mut world: DeferredWorld<'_>, entity: Entity, _component_id: ComponentId) {
+    // Component hooks can't perform structural changes, so we need to rely on commands.
+    world.commands().add(MaybeCommand {
+        entity,
+        _phantom: PhantomData::<B>,
+    });
+}
+
+struct MaybeCommand<B> {
+    entity: Entity,
+    _phantom: PhantomData<B>,
+}
+
+impl<B: Bundle> Command for MaybeCommand<B> {
+    fn apply(self, world: &mut World) {
+        let Some(mut entity_mut) = world.get_entity_mut(self.entity) else {
+            #[cfg(debug_assertions)]
+            panic!("Entity with Maybe component not found");
+
+            #[cfg(not(debug_assertions))]
+            return;
+        };
+
+        let Some(maybe_component) = entity_mut.take::<Maybe<B>>() else {
+            #[cfg(debug_assertions)]
+            panic!("Maybe component not found");
+
+            #[cfg(not(debug_assertions))]
+            return;
+        };
+
+        if let Some(bundle) = maybe_component.into_inner() {
+            entity_mut.insert(bundle);
+        }
     }
 }
